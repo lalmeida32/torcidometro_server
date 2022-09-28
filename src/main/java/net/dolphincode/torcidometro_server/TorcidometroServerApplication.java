8925c79aa4fd4c4c8d86b2e312ccd97b0d387a24
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-09-28 20:44:26 -0300

    chore: spring initializr

---
+++ src/main/java/net/dolphincode/torcidometro_server/TorcidometroServerApplication.java
@@ -0,0 +1,13 @@
+package net.dolphincode.torcidometro_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorcidometroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorcidometroServerApplication.class, args);
+	}
+
+}
