d2227f78af23748e5392c60331950d451329ed91
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2022-09-29 10:54:38 -0300

    refact: change place service tests location

--- src/test/java/net/dolphincode/torcidometro_server/service/PlaceServiceImplTest.java
+++ src/test/java/net/dolphincode/torcidometro_server/service/PlaceServiceImplTest.java
@@ -1,7 +1,39 @@
 package net.dolphincode.torcidometro_server.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import net.dolphincode.torcidometro_server.ApplicationConfigTest;
+import net.dolphincode.torcidometro_server.entity.Place;
+import net.dolphincode.torcidometro_server.repository.PlaceRepository;
+import net.dolphincode.torcidometro_server.request.places.CreatePlaceRequestBody;
+import net.dolphincode.torcidometro_server.response.places.FindPlaceResponse;
 
 public class PlaceServiceImplTest extends ApplicationConfigTest {
+  @MockBean
+  private PlaceRepository placeRepository;
 
+  @Autowired
+  private PlaceService placeService;
+
+  @Test
+  public void shouldCreatePlace() {
+    var place = new Place(1.0, 2.0, "Place Name");
+    Mockito.when(placeRepository.save(ArgumentMatchers.any())).thenReturn(place);
+
+    var request = new CreatePlaceRequestBody(1.0, 2.0, "Place Name");
+    var response = placeService.create(request);
+
+    var expected = new FindPlaceResponse(place.getId(), place.getLatitude(), place.getLongitude(), place.getName());
+    assertEquals(expected.getId(), response.getId());
+    assertEquals(expected.getLatitude(), response.getLatitude());
+    assertEquals(expected.getLongitude(), response.getLongitude());
+    assertEquals(expected.getName(), response.getName());
+
+  }
 }
